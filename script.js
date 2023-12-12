733ec1690688ca932a0e795f8b253ab6994dc4b9
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-12-13 01:36:59 +0700

    Задача

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const phoneInput = document.querySelector('.phone-number');
-const result = '';
+'+7 (___) ___-__-__';
+
+
+// Возвращает строку только с цифрами без букв
+const getCorrectValue = (value) => {
+  return value.split('').filter((item) => (!isNaN(+item)) ? true : false).join('');
+};
+
 
 phoneInput.addEventListener('input', (evt) => {
   evt.preventDefault();
-  evt.target.value.split('').forEach((item) => {
-    if (!isNaN(+item)) {
-      result = result.push(item);
-    }
-  });
-  console.log(result);
+  const input = evt.target;
+  let inputValue = getCorrectValue(evt.target.value);
+  let formattedValue = '';
+
+  console.log(inputValue);
+
+  if (!inputValue) {
+    input.value = '';
+    return;
+  }
+  
+  formattedValue = '+7' + inputValue.slice(1);
+
+  input.value = formattedValue;
 });
