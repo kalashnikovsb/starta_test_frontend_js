629001218bd5618abddee142b912e6559661fcbb
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-12-13 17:12:16 +0700

    Задача

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 const phoneInput = document.querySelector('.phone-number');
-'+7 (___) ___-__-__';
+const mask = '+7 (___) ___-__-__';
 
 
-// Возвращает строку только с цифрами без букв
-const getCorrectValue = (value) => {
-  return value.split('').filter((item) => (!isNaN(+item)) ? true : false).join('');
+const isCorrectSymbol = (symbol) => (!isNaN(+symbol)) ? true : false;
+
+
+const getNumbersValue = (value) => {
+  return value.split('').filter((symbol) => isCorrectSymbol(symbol)).join('');
 };
 
 
-phoneInput.addEventListener('input', (evt) => {
-  evt.preventDefault();
-  const input = evt.target;
-  let inputValue = getCorrectValue(evt.target.value);
+const phoneInputHandler = (evt) => {
+  let input = evt.target;
+  let inputNumbersValue = getNumbersValue(input.value);
   let formattedValue = '';
 
-  console.log(inputValue);
-
-  if (!inputValue) {
+  if (!inputNumbersValue) {
     input.value = '';
     return;
   }
+
+  console.log(inputNumbersValue.length);
   
-  formattedValue = '+7' + inputValue.slice(1);
+  let firstSymbols = '+7(';
+  // if (inputNumbersValue.length === 1) {
+  //   formattedValue += firstSymbols + inputNumbersValue;
+  // }
+  if (inputNumbersValue.length === 2) {
+    formattedValue = '';
+  }
+  if (inputNumbersValue.length >= 1) {
+    formattedValue += firstSymbols + inputNumbersValue.substring(1, 4);
+  }
+  if (inputNumbersValue.length >= 5) {
+    formattedValue += ')' + inputNumbersValue.substring(4, 7);
+  }
+
+  
 
   input.value = formattedValue;
-});
+};
+
+
+phoneInput.addEventListener('input', phoneInputHandler);
