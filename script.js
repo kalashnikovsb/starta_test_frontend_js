9e5e4520c6c766ff89c2888a3fcdc5ecfdcdf3c9
kalashnikovsb <kalashnikov.sk8@gmail.com>
2023-12-14 18:41:43 +0700

    Задача

--- script.js
+++ script.js
@@ -1,77 +1,85 @@
 const phoneInput = document.querySelector('.phone-number');
-const firstSymbols = '+7 (';
-
+isPasted = false;
 
 // Проверка на число
-const isCorrectSymbol = (symbol) => (!isNaN(+symbol) && symbol !== ' ') ? true : false;
+const isNumber = (symbol) => (!isNaN(+symbol) && symbol !== ' ') ? true : false;
 
 
-// Очищает строку от букв и других символов
+// Очищает строку от букв и символов
 const getNumbersValue = (value) => {
-  return value.split('').filter((symbol) => isCorrectSymbol(symbol)).join('');
+  return value.split('').filter((symbol) => isNumber(symbol)).join('');
 };
 
 
-// Нужна для удаления последнего символа
+// Нужна для удаления последнего символа при очистке поля
 const phoneBackspacePressHandler = (evt) => {
   const inputValue = getNumbersValue(evt.target.value);
-  // 2ка это длина строки, когда 1 число это 7ка от маски номера, а второе число последнее оставшееся
+  // 2ка это длина строки, когда первый символ это 7ка от маски номера "+7", а второе число последний символ при удалении
   if (evt.keyCode === 8 && inputValue.length === 2) {
     evt.target.value = '';
   }
 };
 
 
+// Обработчик копирования
 const phonePasteHandler = (evt) => {
-  const text = evt.clipboardData.getData('text');
-  
+  isPasted = true;
+};
+
+
+// Непосредственное форматирование
+const getFormattedData = (inputValue, isPasted) => {
+  const firstSymbols = '+7 (';
+  let result = '';
+
+  // Если значение было вставлено, то первый символ не обрезается
+  // Если ввод начался с 7ки, то +7 не вставляется вначале
+  if (inputValue.length === 1) {
+    result = (inputValue[0] === '7') ? firstSymbols : firstSymbols + inputValue;
+  }
+  if (inputValue.length > 1) {
+    result += firstSymbols + ((isPasted) ? inputValue.substring(0, 3) : inputValue.substring(1, 4));
+  }
+  if (inputValue.length >= 5) {
+    result += ') ' + ((isPasted) ? inputValue.substring(3, 6) : inputValue.substring(4, 7));
+  }
+  if (inputValue.length >= 8) {
+    result += '-' + ((isPasted) ? inputValue.substring(6, 8) : inputValue.substring(7, 9));
+  }
+  if (inputValue.length >= 10) {
+    result += '-' + ((isPasted) ? inputValue.substring(8, 10) : inputValue.substring(9, 11));
+  }
+
+  return result;
 };
 
 
 const phoneInputHandler = (evt) => {
   let input = evt.target;
-  console.log(evt.target.value);
-  let inputNumbersValue = getNumbersValue(input.value);
-  let formattedValue = '';
+  // let inputValue = evt.target.value;
+  let numbersValue = getNumbersValue(input.value);
   const selectionStart = input.selectionStart;
 
-  if (!inputNumbersValue) {
+  if (!numbersValue) {
     input.value = '';
     return;
   }
 
   if (input.value.length !== selectionStart) {
-    if (!isCorrectSymbol(evt.data)) {
-      input.value = inputNumbersValue;
+    if (!isNumber(evt.data)) {
+      input.value = numbersValue;
     }
     return;
   }
 
-  // console.log(inputNumbersValue, inputNumbersValue.length);
-  
-  if (inputNumbersValue.length === 1) {
-    formattedValue = firstSymbols + inputNumbersValue;
-  }
-  if (inputNumbersValue.length > 1) {
-    formattedValue += firstSymbols + inputNumbersValue.substring(1, 4);
-  }
-  if (inputNumbersValue.length >= 5) {
-    formattedValue += ') ' + inputNumbersValue.substring(4, 7);
-  }
-  if (inputNumbersValue.length >= 8) {
-    formattedValue += '-' + inputNumbersValue.substring(7, 9);
-  }
-  if (inputNumbersValue.length >= 10) {
-    formattedValue += '-' + inputNumbersValue.substring(9, 11);
+  // Нужно чтобы при вставке полного номера не добавлялось +7, 7, 8 к маске
+  if (numbersValue.length > 10 && isPasted && (evt.target.value.substring(0, 2) === '+7' || evt.target.value[0] === '7' || evt.target.value[0] === '8')) {
+    numbersValue = numbersValue.substring(1);
   }
 
-  console.log(formattedValue);
-  input.value = formattedValue;
-};
-
-
-const getFormattedData = () => {
-  
+  const formattedData = getFormattedData(numbersValue, isPasted);
+  isPasted = false;
+  input.value = formattedData;
 };
 
 
